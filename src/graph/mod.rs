a7037079f93b5bbacbcd70fe61fa11da63b9fc09
Igor Pietrzak <igor.pietrzak70@gmail.com>
2024-10-17 20:21:49 +0100

    Finilised getting degree based OPP in graph impl.

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::opp::OPP;
 
 // use an adjacency matrix as it is more effcient than a hashmap.
@@ -10,16 +12,23 @@ impl Graph {
         Graph { adj_mat }
     }
 
-    // degree based pruning decides what OPP we begin with.
-    // pub fn get_opp(&self) -> OPP {
-    //     let set: HashSet<usize> = HashSet::new();
-    //     for i in 0..self.adj_mat.len() {
-    //         set.insert(i);
-    //     }
-    //     let compatibility_vec = self.sort_by_degree();
-    // }
+    // degree based pruning decides what OPP we begin with. O(n)
+    pub fn get_opp(&self) -> OPP {
+        let mut set: HashSet<usize> = HashSet::new();
+        for i in 0..self.adj_mat.len() {
+            set.insert(i);
+        }
+        let compatibility_vec = self.sort_by_degree();
+        let mut top: Vec<Vec<usize>> = Vec::new();
+        let mut bottom: Vec<Vec<usize>> = Vec::new();
+        for i in compatibility_vec {
+            top.push(i.clone());
+            bottom.push(i);
+        }
+        OPP::new(set, top, bottom)
+    }
 
-    pub fn sort_by_degree(&self) -> Vec<Vec<usize>> {
+    fn sort_by_degree(&self) -> Vec<Vec<usize>> {
         let mut seen: Vec<usize> = Vec::new();
         let mut compatibility_vec: Vec<Vec<usize>> = Vec::new();
 
@@ -34,6 +43,7 @@ impl Graph {
 
             for j in 0..self.adj_mat.len() {
                 if !seen.contains(&j) {
+                    // .contains() is O(n)
                     match self.adj_mat[i].len() == self.adj_mat[j].len() {
                         true => {
                             seen.push(j);
@@ -50,12 +60,12 @@ impl Graph {
     }
 }
 
-#[cfg(test)]
-mod test {
-    use super::Graph;
-    #[test]
-    fn test_sorting_by_degree() {
-        let graph = Graph::new(vec![vec![1], vec![0, 2], vec![1]]);
-        println!("sorted {:?}", graph.sort_by_degree());
-    }
-}
+// #[cfg(test)]
+// mod test {
+//     use super::Graph;
+//     #[test]
+//     fn test_sorting_by_degree() {
+//         let graph = Graph::new(vec![vec![1], vec![0, 2], vec![1]]);
+//         println!("sorted {:?}", graph.sort_by_degree());
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 use opp::graph::Graph;
-use std::collections::HashSet;
 
 // SAUCY PAPER: http://alcom.ee.ntu.edu.tw/system/privatezone/meetingfile/201210041839101.pdf
 
 fn main() {
-    let mut set = HashSet::new();
-    set.insert(0);
-    set.insert(1);
-    set.insert(2);
-
     let graph = Graph::new(vec![vec![1, 2], vec![0, 2], vec![1, 0]]);
-    println!("{:?}", graph);
+    let opp = graph.get_opp();
+    opp.print();
 }
